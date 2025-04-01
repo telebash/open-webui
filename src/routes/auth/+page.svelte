083d14dd60cd46c94d95e51b66b3ed308604ddee
Yerassyl <fake_era@mail.ru>
2025-04-01 23:14:05 +0500

    FIX

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -209,7 +209,7 @@
 			class="fixed bg-transparent min-h-screen w-full flex justify-center font-primary z-50 text-black dark:text-white"
 		>
 			<div class="w-full sm:max-w-md px-10 min-h-screen flex flex-col text-center">
-				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false || ($user === undefined)}
+				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false}
 					<div class=" my-auto pb-10 w-full">
 						<div
 							class="flex items-center justify-center gap-3 text-xl sm:text-2xl text-center font-semibold dark:text-gray-200"
