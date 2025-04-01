fc1bab31995335f1163fa203eb2a69e6fc827c7b
Yerassyl <fake_era@mail.ru>
2025-04-01 23:06:35 +0500

    FIX

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -162,9 +162,7 @@
 
 		if (hasSecretAuth) {
 			onboarding = $config?.onboarding ?? false;
-			loaded = false;
-		}
-		else if (($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false) {
+		} else if (($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false) {
 			await signInHandler();
 		} else {
 			// onboarding = $config?.onboarding ?? false;
@@ -211,7 +209,7 @@
 			class="fixed bg-transparent min-h-screen w-full flex justify-center font-primary z-50 text-black dark:text-white"
 		>
 			<div class="w-full sm:max-w-md px-10 min-h-screen flex flex-col text-center">
-				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false || (hasSecretAuth === true && $user === undefined)}
+				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false || ($user === undefined)}
 					<div class=" my-auto pb-10 w-full">
 						<div
 							class="flex items-center justify-center gap-3 text-xl sm:text-2xl text-center font-semibold dark:text-gray-200"
